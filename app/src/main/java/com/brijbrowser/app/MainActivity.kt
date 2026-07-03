124154a56bf5b56c53868bf280e44bd13901f3f4
BrijeshPrajapati3433 <bjprj112233400@gmail.com>
2026-07-03 13:11:38 +0530

    Add MainActivity class for the app

---
+++ app/src/main/java/com/brijbrowser/app/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.brijbrowser.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
